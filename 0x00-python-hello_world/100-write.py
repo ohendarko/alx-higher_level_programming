69979cd1d0ad7a9d2814196cecb5be2fd1b0a688
ohendarko <ohendarko@gmail.com>
2023-10-02 22:04:22 +0000

    submit

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 import sys
+
+
 def main():
-	output = "and that piece of art is useful - Dora Korpar, 2015-10-19"
-	sys.stderr.write(output + "\n")
-	sys.exit(1)
+    output = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+
+    sys.stderr.write(output + "\n")
+
+    sys.exit(1)
+
+
 if __name__ == "__main__":
-	main()
+    main()
