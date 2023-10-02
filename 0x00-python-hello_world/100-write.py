e230cbcaaa8e5cfd8668074ce4ec4dcb3bb27646
ohendarko <ohendarko@gmail.com>
2023-10-02 21:58:17 +0000

    submit

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+def main():
+	output = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+	sys.stderr.write(output + "\n")
+	sys.exit(1)
+if __name__ == "__main__":
+	main()
