2d9ccd376e9d5871fcce12208fde888fade113c1
ohendarko <ohendarko@gmail.com>
2023-10-05 21:37:00 +0000

    submit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-summ = add(a, b)
-print("{} + {} = {}".format(a, b, summ))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    summ = add(a, b)
+    print("{} + {} = {}".format(a, b, summ))
