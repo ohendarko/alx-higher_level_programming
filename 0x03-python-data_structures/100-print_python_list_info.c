dc2f02b2c008ef9e11194cae728e126c07d4ec38
ohendarko <ohendarko@gmail.com>
2023-10-08 21:22:41 +0000

    submit

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -7,7 +7,7 @@
 void print_python_list_info(PyObject *p)
 {
 	int numof_elem = Py_SIZE(p);
-	int allo_sz = ((PyListObject*)p)->allocated;
+	int allo_sz = ((PyListObject *)p)->allocated;
 	int i;
 	PyObject *obj;
 
