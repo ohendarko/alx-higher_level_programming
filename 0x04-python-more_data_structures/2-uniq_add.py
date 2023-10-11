79e893907e58639458846378dbc551fe0abfc3bc
ohendarko <ohendarko@gmail.com>
2023-10-11 12:46:50 +0000

    submit

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     sm = 0
-    uniq_list = my_list.copy()
-    for i in uniq_list:
-        if uniq_list.count(i) > 1:
-            uniq_list.remove(i)
+    uniq_list = list(set(my_list))
     for i in range(len(uniq_list)):
         sm += uniq_list[i]
     return sm
