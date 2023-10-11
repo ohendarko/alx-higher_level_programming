bf60ff9f670b0d8e12c29f9fc4923a776fcf6795
ohendarko <ohendarko@gmail.com>
2023-10-11 12:44:38 +0000

    submit

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from functools import reduce
 def uniq_add(my_list=[]):
+    sm = 0
     uniq_list = my_list.copy()
     for i in uniq_list:
         if uniq_list.count(i) > 1:
             uniq_list.remove(i)
-    return (reduce((lambda x, y: x + y), uniq_list))
+    for i in range(len(uniq_list)):
+        sm += uniq_list[i]
+    return sm 
