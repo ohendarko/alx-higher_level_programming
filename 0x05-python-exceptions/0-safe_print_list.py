22e4b7f660991659c918335db3344fb697a38972
ohendarko <ohendarko@gmail.com>
2023-10-23 21:39:23 +0000

    submit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             nb_print += 1
         print()
-    except:
+    except IndexError:
         print()
-    return nb_print            
+    return nb_print
