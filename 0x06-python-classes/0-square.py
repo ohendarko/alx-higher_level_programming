81fd90d54f0c02606736c083a04dc76abe955264
ohendarko <ohendarko@gmail.com>
2023-10-24 21:06:11 +0000

    submit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Each module, class, and
+method must contain docstring as comments."""
+
+
+class Square:
+    """ Each module, class, and
+    method must contain docstring as comments."""
+    pass
