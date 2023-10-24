80d7536ed9fb3bc8c54d2d6281cd9994f932897c
ohendarko <ohendarko@gmail.com>
2023-10-24 22:34:07 +0000

    submit

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Calculating area and circumference
+"""
+from math import pi
+
+
+class MagicClass:
+    """ calculating area and circumference """
+    def __init__(self, radius=0):
+        """ calculating area and circumference """
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.radius = radius
+
+    def area(self):
+        """ calculating area and circumference """
+        return self.__radius ** 2 * pi
+
+    def circumference(self):
+        """ calculating area and circumference """
+        return 2 * pi * self.__radius
