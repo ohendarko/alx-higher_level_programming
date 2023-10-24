87744437433af85751a52b0d49a64710fe887ba0
ohendarko <ohendarko@gmail.com>
2023-10-24 21:20:02 +0000

    submit

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Each module, class, and
+method must contain docstring as comments."""
+
+
+class Square:
+    """ Each module, class, and
+    method must contain docstring as comments."""
+    def __init__(self, size=0):
+        """ Each module, class, and
+        method must contain docstring as comments."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
