c2fede3b3a5dc18a66eb2e3f3ef50ba22819e3cf
ohendarko <ohendarko@gmail.com>
2023-11-08 04:34:19 +0000

    submit

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module documentation
+"""
+import json
+
+
+def pascal_triangle(n):
+    """Function documentation
+    """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            element = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            row.append(element)
+        row.append(1)
+        triangle.append(row)
+    return triangle
