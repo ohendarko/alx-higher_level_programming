8eded4dafdcc069c6d601b5ba17699bd25158d6c
ohendarko <ohendarko@gmail.com>
2023-11-08 04:09:25 +0000

    submit

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 def load_from_json_file(filename):
     """Function documentation
     """
-    with open(filename, 'w', encoding='utf-8') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         return json.load(file)
