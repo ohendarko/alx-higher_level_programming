ec10380902eb48104ed5bdc3979fceb887cf837a
ohendarko <ohendarko@gmail.com>
2023-12-13 12:49:30 +0000

    submit

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,6 @@
 #!/bin/bash
 -- creates a table called first_table in the current database in your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS first_table(
 		id INT
