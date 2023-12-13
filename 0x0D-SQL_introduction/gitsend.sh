9c3ed39f2274955af6bc0aa948b6b2398bf73bc7
ohendarko <ohendarko@gmail.com>
2023-12-13 13:47:17 +0000

    submit

--- 0x0D-SQL_introduction/gitsend.sh
+++ 0x0D-SQL_introduction/gitsend.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# Check if a commit message is provided as an argument
+if [ -z "$1" ]; then
+  commit_message="submit"
+else
+  commit_message="$1"
+fi
+
 git add .
-git commit -m 'submit'
+git commit -m "$commit_message"
 git push
