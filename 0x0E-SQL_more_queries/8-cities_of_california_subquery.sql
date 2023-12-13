a008062987683fd50f7c8c0cf05285aa3319bb13
ohendarko <ohendarko@gmail.com>
2023-12-13 20:51:09 +0000

    submit

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,6 @@
 -- 8-cities_of_california_subquery.sql
 USE hbtn_0d_usa;
 SELECT id, name FROM cities WHERE cities.id = (
-	SELECT states.id FROM states
-	WHERE states.name = 'California'
+	SELECT states.id FROM states WHERE states.name = 'California'
 )
 ORDER BY cities.id;
