274b6cf47566fa18458f10f95ee85aa0547c242f
ohendarko <ohendarko@gmail.com>
2023-12-13 11:58:30 +0000

    submit

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,5 +1,3 @@
 #!/bin/bash
 --  lists all databases of MySQL server.
-USER="root"
-PASSWORD="root"
-databases=$(mysql -u"$USER" -p"$PASSWORD" -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)")
+SHOW DATABASES;
