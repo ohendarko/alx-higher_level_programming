af88a3f039831da9307988da1976dc21bffd51ad
ohendarko <ohendarko@gmail.com>
2023-12-13 21:05:51 +0000

    submit

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
--- 8-cities_of_california_subquery.sql
+-- lists all cities contained in the database hbtn_0d_usa.
+USE hbtn_0d_usa;
 SELECT id, name FROM cities WHERE state_id = (
 	SELECT id FROM states WHERE name = 'California'
 )
