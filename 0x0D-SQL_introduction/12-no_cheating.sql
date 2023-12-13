9fcf329563bfa7e24d3e64ef23b5c6b815126901
ohendarko <ohendarko@gmail.com>
2023-12-13 13:23:20 +0000

    submit

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+#!/bin/bash
+-- lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
