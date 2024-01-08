c7833064c146fee3e018fb7e071b1944144fdabb
ohendarko <ohendarko@gmail.com>
2024-01-08 23:00:13 +0000

    submit

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const num = parseInt(args[0]);
+
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n <= 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+console.log(factorial(num));
