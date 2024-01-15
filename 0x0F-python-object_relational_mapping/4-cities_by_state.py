647876f930cbff307c6f1513e2d253fa9d4cf0b8
ohendarko <ohendarko@gmail.com>
2024-01-15 02:19:14 +0000

    submit

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""a script that lists all cities
+from the database hbtn_0e_4_usa"""
+import MySQLdb
+import sys
+
+
+def list_cities(username, password, database_name):
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database_name)
+    cursor = db.cursor()
+    query = "SELECT * FROM cities ORDER BY cities.id ASC"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ")
+        sys.exit(1)
+    username, password, database_name = sys.argv[1:4]
+    list_cities(username, password, database_name)
