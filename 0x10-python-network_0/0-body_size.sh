6860a385920823785f351bf3cd16d6076b04f6b4
ohendarko <ohendarko@gmail.com>
2024-01-24 09:10:06 +0000

    submit

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Takes in a URL, sends a request to that URL, and displays the size of the body of the response
+
+url=$1
+
+response_size=$(curl -sI "$url" | grep -i content-length | awk '{print $2}' | tr -d '\r')
+
+echo $response_size
