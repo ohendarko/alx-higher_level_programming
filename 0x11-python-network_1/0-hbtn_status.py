1f86676faf5744ad4d2fab60cf439b3f98e5f8ed
ohendarko <ohendarko@gmail.com>
2024-01-24 15:35:04 +0000

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,10 @@
 https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
-    page = resp.read()
-print(f'''Body response:
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        page = resp.read()
+    print(f'''Body response:
 	- type: {type(page)}
 	- content: {page}
 	- utf8 content: {page.decode()}''')
