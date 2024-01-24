7679ff2938158f23dd16c731dab5adc8610df392
ohendarko <ohendarko@gmail.com>
2024-01-24 15:27:32 +0000

    submit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
     page = resp.read()
 print(f'''Body response:
-    - type: {type(page)}
-    - content: {page}
-    - utf8 content: {page.decode()}''')
+	- type: {type(page)}
+	- content: {page}
+	- utf8 content: {page.decode()}''')
