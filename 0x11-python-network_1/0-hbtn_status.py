b2b45d517288f5b786ca0a0e5808e859a61164f0
ohendarko <ohendarko@gmail.com>
2024-01-24 15:46:15 +0000

    submit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         page = resp.read()
     print("Body of the response:")
-    print("\t- type:", type(page))
-    print("\t- content:", page)
-    print("\t- utf8 content:", page.decode("utf-8"))
+    print("\t\t- type:", type(page))
+    print("\t\t- content:", page)
+    print("\t\t- utf8 content:", page.decode("utf-8"))
