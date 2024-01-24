803df858e9713b24130f7599028d59b7135aed47
ohendarko <ohendarko@gmail.com>
2024-01-24 10:15:52 +0000

    submit

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,11 +2,12 @@
 """a function that finds a peak
 in a list of unsorted integers."""
 
+
 def find_peak(list_of_integers):
     """finds a peak in a list of unsorted integers."""
     if not list_of_integers:
         return None
-    
+
     low, high = 0, len(list_of_integers) - 1
 
     while low < high:
@@ -14,5 +15,5 @@ def find_peak(list_of_integers):
         if list_of_integers[mid] > list_of_integers[mid + 1]:
             high = mid
         else:
-            low = mid +1
+            low = mid + 1
     return list_of_integers[low]
