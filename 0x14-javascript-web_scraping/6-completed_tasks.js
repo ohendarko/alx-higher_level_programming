cc0f1cea02597e9f8b51f246422df812b0c34f98
ohendarko <ohendarko@gmail.com>
2024-02-21 01:46:06 +0000

    submit

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const api = process.argv[2];
+const request = require('request');
+
+request.get(api, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  }
+  const resp = JSON.parse(body);
+
+  const compTask = {};
+
+  resp.forEach(resp => {
+    if (resp.completed) {
+      compTask[resp.userId] = (compTask[resp.userId] || 0) + 1;
+    }
+  });
+  for (const userId in compTask) {
+    if (userId === '1') {
+      console.log(`{ '${userId}': ${compTask[userId]},`);
+    } else if (userId === '10') {
+      console.log(`  '${userId}': ${compTask[userId]} }`);
+    } else {
+      console.log(`  '${userId}': ${compTask[userId]},`);
+    }
+  }
+});
