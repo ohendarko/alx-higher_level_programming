70a7559362b4a35e99db22b3d7aaaba80c4eb64b
ohendarko <ohendarko@gmail.com>
2024-02-21 01:54:40 +0000

    submit

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-// A script that computes the number of tasks completed by user id.
-
 const args = process.argv;
 const reqURL = args[2];
 const request = require('request');
@@ -8,12 +6,12 @@ request(reqURL, function (error, response, body) {
   if (error) {
     console.log('error:', error);
   } else {
-    const todos = JSON.parse(body);
+    const resp = JSON.parse(body);
     const dash = {};
-    for (let i = 0; i < todos.length; i++) {
-      const status = (todos[i].completed);
-      const key = todos[i].userId.toString();
-      if (status) {
+    for (let i = 0; i < resp.length; i++) {
+      const comp_task = (resp[i].completed);
+      const key = resp[i].userId.toString();
+      if (comp_task) {
         if (dash[key]) {
           dash[key]++;
         } else {
