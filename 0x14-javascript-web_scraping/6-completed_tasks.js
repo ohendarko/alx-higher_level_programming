0c52d73741f97d8672f62b61f382eb888defdfde
ohendarko <ohendarko@gmail.com>
2024-02-21 01:48:01 +0000

    submit

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -19,9 +19,9 @@ request.get(api, (err, response, body) => {
     if (userId === '1') {
       console.log(`{ '${userId}': ${compTask[userId]},`);
     } else if (userId === '10') {
-      console.log(`  '${userId}': ${compTask[userId]} }`);
+      console.log(`'${userId}': ${compTask[userId]} }`);
     } else {
-      console.log(`  '${userId}': ${compTask[userId]},`);
+      console.log(`'${userId}': ${compTask[userId]},`);
     }
   }
 });
